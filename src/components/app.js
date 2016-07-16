3880f17c6cb3a109c695f4867a7b9145a64d5cfd
Renaud Tertrais <renaud.tertrais@gmail.com>
2016-07-16 20:22:28 +0200

    save viewer pane size in local storage

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import SplitPane from 'react-split-pane';
+import { local } from 'store2';
 import Editor from './editor';
 import Viewer from './viewer';
 
+const storedSizeViewerPane = local.get('size_viewer_pane');
+
 export default class App extends Component {
   render() {
-    const width = window.innerHeight;
+
+    const defaultSize = storedSizeViewerPane || window.innerWidth * 0.3;
 
     return (
-      <SplitPane split="vertical" defaultSize={width}>
+      <SplitPane
+         split="vertical"
+         defaultSize={defaultSize}
+         primary="second"
+         onChange={local.set.bind(local, 'size_viewer_pane')}
+      >
         <Editor />
         <Viewer />
       </SplitPane>
