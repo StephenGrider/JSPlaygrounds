9c4ae645fd0d57b455020c4b6252cc77c7737adc
stephen grider <ste.grider@gmail.com>
2018-10-30 13:38:26 -0600

    transform on window

--- src/globals.js
+++ src/globals.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { createStore, combineReducers } from 'redux';
+import { transform } from 'babel-standalone';
 const tf = require('@tensorflow/tfjs');
 
 _.extend(window, {
+  transform: code => transform(code, { presets: ['react'] }).code,
   _,
   React,
   Component,
